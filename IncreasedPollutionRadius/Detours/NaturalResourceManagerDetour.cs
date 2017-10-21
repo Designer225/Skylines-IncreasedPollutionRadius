68b99bf083da16daa3e034d84a98b05a7c7b03bf
Egor Aralov <aei.omsk@gmail.com>
2017-10-22 00:10:45 +0200

    Remove experimental features

--- IncreasedPollutionRadius/Detours/NaturalResourceManagerDetour.cs
+++ IncreasedPollutionRadius/Detours/NaturalResourceManagerDetour.cs
@@ -10,10 +10,11 @@ namespace IncreasedPollutionRadius.Detours
     [TargetType(typeof(NaturalResourceManager))]
     public class NaturalResourceManagerDetour : NaturalResourceManager
     {
+        private static bool DEBUG_OUTPUT = false;
+
         [RedirectMethod]
-        public new int TryDumpResource(NaturalResourceManager.Resource resource, int rate, int max, Vector3 position,
-            float radius)
-        {
+        public int TryDumpResource(NaturalResourceManager.Resource resource, int rate, int max, Vector3 position, float radius, bool refresh)
+        {    
             //begin mod
             if (resource == Resource.Pollution)
             {
@@ -21,7 +22,7 @@ namespace IncreasedPollutionRadius.Detours
                 {
                     radius = LoadingExtension.OriginalFactor * radius;
                 }
-                var rateFactor = LoadingExtension.OriginalFactor * LoadingExtension.OriginalFactor;
+                var rateFactor = LoadingExtension.OriginalFactor /** LoadingExtension.OriginalFactor*/;
                 rate = rate * rateFactor;
                 max = max * rateFactor;
             }
@@ -29,32 +30,34 @@ namespace IncreasedPollutionRadius.Detours
             {
                 radius = Mathf.Max(33.75f, (float)Singleton<SimulationManager>.instance.m_randomizer.Int32(200, 1000) * (radius * (1f / 1000f)));
             }
-
             //end mod
             int numCells;
             int totalCells;
             int resultDelta;
-            CountResource(resource, position, radius, 0, out numCells, out totalCells, out resultDelta);
+            this.CountResource(resource, position, radius, 0, out numCells, out totalCells, out resultDelta, false);
             rate = Mathf.Min(rate, max);
-            if (resource == Resource.Pollution && PollutionRuntimeOptions.instance.DebugOutput)
+            //begin mod
+            if (resource == Resource.Pollution && DEBUG_OUTPUT)
             {
                 UnityEngine.Debug.Log($"#1: position={position}, radius={radius}, numCells={numCells}, totalCells={totalCells}, resultDelta={resultDelta}, rate={rate}");
             }
+            //end mod
             if (totalCells == 0 || rate == 0)
                 return 0;
             int cellDelta = (int)(((long)rate << 20) / (long)totalCells);
-            CountResource(resource, position, radius, cellDelta, out numCells, out totalCells, out resultDelta);
-            if (resource == Resource.Pollution && PollutionRuntimeOptions.instance.DebugOutput)
+            this.CountResource(resource, position, radius, cellDelta, out numCells, out totalCells, out resultDelta, refresh);
+            //begin mod
+            if (resource == Resource.Pollution && DEBUG_OUTPUT)
             {
                 UnityEngine.Debug.Log($"#2: position={position}, cellDelta={cellDelta}, numCells={numCells}, totalCells={totalCells}, resultDelta={resultDelta}");
             }
+            //end mod
             return rate;
         }
 
         [RedirectReverse]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        private int CountResource(NaturalResourceManager.Resource resource, Vector3 position, float radius,
-            int cellDelta, out int numCells, out int totalCells, out int resultDelta)
+        private int CountResource(NaturalResourceManager.Resource resource, Vector3 position, float radius, int cellDelta, out int numCells, out int totalCells, out int resultDelta, bool refresh)
         {
             UnityEngine.Debug.LogError("failed to detour CountResource()");
             numCells = 0;
